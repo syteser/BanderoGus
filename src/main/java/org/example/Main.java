71d53ee4bf783f46ad3e2ec360c06547c333e110
syteser <savchenko.sergey.v@gmail.com>
2023-06-26 21:51:59 +0300

    first version with GitHub 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main extends TelegramLongPollingBot {
     public final String MY_NAME = "Sergey";
+
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
         api.registerBot(new Main());
@@ -28,22 +29,22 @@ public class Main extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         Long chatID = getChatID(update);
 
-        SendMessage msg = createMessage("*Hello* "+ MY_NAME+ "!!!");
+        SendMessage msg = createMessage("*Hello* " + MY_NAME + "!!!");
         msg.setChatId(chatID);
         sendApiMethodAsync(msg);
     }
 
-    public Long getChatID(Update update){
-        if(update.hasMessage()){
+    public Long getChatID(Update update) {
+        if (update.hasMessage()) {
             return update.getMessage().getFrom().getId();
         }
-        if(update.hasCallbackQuery()){
+        if (update.hasCallbackQuery()) {
             return update.getCallbackQuery().getFrom().getId();
         }
         return null;
     }
 
-    public SendMessage createMessage(String text){
+    public SendMessage createMessage(String text) {
         SendMessage message = new SendMessage();
         message.setText(text);
         message.setParseMode("markdown");
