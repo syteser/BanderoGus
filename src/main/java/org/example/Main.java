4782403bb2a107ea39e2c797c45a49165b4cf0d0
syteser <savchenko.sergey.v@gmail.com>
2023-06-26 20:16:58 +0300

    first version with GitHub 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main extends TelegramLongPollingBot {
-    int i= 6;
     public final String MY_NAME = "Sergey";
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
